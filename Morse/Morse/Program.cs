81db89ca8f9c5c1ca6a75fdef809f1f4d89346ce
Alejandro <ale_arc2015@hotmail.com>
2019-06-13 02:40:26 -0300

    Progress

--- Morse/Morse/Program.cs
+++ Morse/Morse/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using Helpers;
 
 namespace Morse
 {
@@ -10,6 +12,25 @@ namespace Morse
     {
         static void Main(string[] args)
         {
+            string[] archivos_txt;
+            string[] archivos_mp3;
+            SoporteParaConfiguracion.CrearArchivoDeConfiguracion();
+            string path = SoporteParaConfiguracion.LeerConfiguracion();
+            archivos_txt = Directory.GetFiles(".", "*.txt");
+            archivos_mp3 = Directory.GetFiles(".", "*.mp3");
+
+            foreach (string arch in archivos_txt)
+            {
+                File.Move(arch,path + arch);
+            }
+            foreach (string arch in archivos_mp3)
+            {
+                File.Move(arch, path + arch);
+            }
+
+
+
+
         }
     }
 }
---
+++ Morse/Morse/SoporteParaConfiguracion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    class SoporteParaConfiguracion
+    {
+        ///creo un archivo binario para cargar la ruta
+        public static void CrearArchivoDeConfiguracion()
+        {
+            BinaryWriter bw = new BinaryWriter(File.Open("Config.dat", FileMode.Create));
+            bw.Write(@"F:\Taller\skere");
+            Console.WriteLine("Archivo creado.");
+            bw.Close();
+        }
+        ///leo un archivo de config 
+        public static string LeerConfiguracion()
+        {
+            string Path;
+            BinaryReader br = new BinaryReader(File.Open("Config.dat", FileMode.Open));
+            Path = br.ReadString();
+            Console.WriteLine("{0}",Path);
+            return Path;
+        }       
+    }
+}
